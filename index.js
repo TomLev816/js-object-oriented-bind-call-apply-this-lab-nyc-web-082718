7945b954c6a08975bb603e961d02ac9131da16d8
github username <github email address>
2018-10-11 08:08:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Your code here
+// ok
+
+const justInvoke = function(fn) {
+  return fn()
+}
+
+const setThisWithCall = function(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+}
+
+const setThisWithApply = function (returnsThisAndArgs, bob, [age, hairColor]) {
+  return returnsThisAndArgs.apply(bob, [age, hairColor])
+}
+
+const returnNewFunctionOf = function (functionToBeCopied, fred) {
+  return functionToBeCopied.bind(fred)
+}
+
+
+// returnsThisAndArgs = function(){ return {thisValue: this, arguments: Array.from(arguments)} }
+// bob = {name: 'bob'}
+// age = 18
+// })
